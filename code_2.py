d4312924a97d74c6425e9fbbf1eb718d21139480
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-22 16:11:13 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+    public static void main(String[] args) {
+    }
+        int[] data = new int[10];
+public class RandomDataGenerator {
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+
+}
+            System.out.println("Random Number: " + item);
